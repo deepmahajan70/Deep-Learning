55fa3fed7b098696960de8cb1790c8538fe9a3b3
Deepak Mahajan <deepmahajan70@gmail.com>
2023-07-25 10:24:02 +0530

    Add files via upload

---
+++ rnn.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the training set
+dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
